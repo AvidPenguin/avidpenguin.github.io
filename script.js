b28177611820f69b3e15caba76ff606912ffe2d6
Dan <avidpenguin@gmail.com>
2019-01-25 15:11:51 +0000

    Log ins, Reviews and UI updates

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var users = [];
 var movies = [];
-var showUnreleased = true;
+var reviews = [];
+
+var movieCount = 0;
+
+var searchKey = "";
+var search = false;
+var sortOption = 0;
+var user = "";
 
 // Initialize Firebase
 var config = {
@@ -20,7 +27,7 @@ firebase.database().ref().child("users").once("value", function (snapshot) {
 			// 
 			data = snapshot2.val();
 			// 
-			var newUser = new User(data.name, data.email, data.pass);
+			var newUser = new User(data.username, data.email, data.password);
 			// 
 			users.push(newUser);
 		});
@@ -31,6 +38,8 @@ firebase.database().ref().child("movies").once("value", function (snapshot3) {
 	snapshot3.forEach(function (child) {   
 		firebase.database().ref('/movies/' + child.key).once('value').then(function (snapshot4) {
 			// 
+			movieCount++;
+			//
 			data = snapshot4.val();
 			// 
 			var date = new Date(data.release.substring(6,12),data.release.substring(3,5),data.release.substring(0,2));
@@ -38,10 +47,26 @@ firebase.database().ref().child("movies").once("value", function (snapshot3) {
 			var newMovie = new Movie(child.key, data.title, data.director, date, data.imdb, data.image);
 			movies.push(newMovie);
 			//
-			UpdateMovieList();
+			UpdateUI();
 		});
 	})
 });
+firebase.database().ref().child("reviews").once("value", function (snapshot5) {
+	// For each movie in the database
+	snapshot5.forEach(function (child) {   
+		firebase.database().ref('/reviews/' + child.key).once('value').then(function (snapshot6) {
+			// 
+			data = snapshot6.val();
+			//
+			var newReview = new Review(child.key, data.movie, data.rating, data.review, data.user);
+			reviews.push(newReview);
+			//
+			UpdateUI();
+		});
+	})
+});
+
+
 
 
 function User(n,e,p) // Username, Email, Password
@@ -61,43 +86,171 @@ function Movie(id,t,d,r,im,img) // ID, Title, Director, Release Date (DD/MM/YYYY
 	this.image = img;
 }
 
-function UpdateMovieList()
+function Review(id,m,r,t,u) // ID, Movie, Rating, Review, User
 {
+	this.id = id;
+	this.movie = m;
+	this.rating = r;
+	this.review = t;
+	this.user = u;
+}
+
+function UpdateUI()
+{
+	if(user == "") // Not logged in
+	{
+		document.getElementById("navNewMovie").classList.add("hidden");
+		document.getElementById("navNewReview").classList.add("hidden");
+		document.getElementById("navLogIn").classList.remove("hidden");
+		document.getElementById("navLogOut").classList.add("hidden");
+	}
+	else if (user.name == "AvidPenguin" || user.name == "GeoNugget" || user.name == "Admin")
+	{
+		document.getElementById("navNewMovie").classList.remove("hidden");
+		document.getElementById("navNewReview").classList.remove("hidden");
+		document.getElementById("navLogIn").classList.add("hidden");
+		document.getElementById("navLogOut").classList.remove("hidden");
+		document.getElementById("navLIA").innerHTML = "Logged in as " + user.name;
+	}
+	else
+	{
+		document.getElementById("navNewMovie").classList.add("hidden");
+		document.getElementById("navNewReview").classList.remove("hidden");
+		document.getElementById("navLogIn").classList.add("hidden");
+		document.getElementById("navLogOut").classList.remove("hidden");
+		document.getElementById("navLIA").innerHTML = "Logged in as " + user.name;
+	}
+	
 	document.getElementById("movieList").innerHTML = "";
 	
 	if(movies.length > 0)
 	{
 		
-		//var select = document.getElementById("newReviewSelect");
-		//select.options.length = 0;
 		
-		movies.sort((a,b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0));
+		var select = document.getElementById("newReviewMovie");
+		select.options.length = 0;
 		
 		for (i = 0; i < movies.length; i++) //  For each movie in the database
 		{
-				//select.options[select.options.length] = new Option(movies[i].title, movies[i].id);
+				select.options[select.options.length] = new Option(movies[i].title, movies[i].id);
 		}
-		movies.sort((a,b) => (a.release > b.release) ? 1 : ((b.release > a.release) ? -1 : 0));
 		
-		for (i = 0; i < movies.length; i++) //  For each movie in the database
-		{		
-			document.getElementById("movieList").innerHTML +=
-				"<div class='movieWrapper'><img class='cover' src='" +
-				movies[i].image +
-				"' alt='" +
-				movies[i].title +  
-				" Cover'><p class='movieTitle'>" +
-				movies[i].title +
-				"</p><p class='movieText'>Director: " + 
-				movies[i].director +
-				"<br>Released: " + 
-				movies[i].release.toDateString().substring(4) +
-				"</p><a href='" + 
-				movies[i].imdb +
-				"' class='btn btn-primary' target='_blank'>IMDB Link</a><span> </span><button class='btn btn-primary' onclick=\"AddReview('" + 
-				movies[i].id +
-				"')\">Add Review</button></div><span> </span>";
 		
+		if(sortOption==0)
+		{
+			movies.sort((a,b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0));
+		}
+		if(sortOption==1)
+		{
+			movies.sort((a,b) => (a.title < b.title) ? 1 : ((b.title < a.title) ? -1 : 0));
+		}
+		if(sortOption==2)
+		{
+			movies.sort((a,b) => (a.release > b.release) ? 1 : ((b.release > a.release) ? -1 : 0));
+		}
+		if(sortOption==3)
+		{
+			movies.sort((a,b) => (a.release < b.release) ? 1 : ((b.release < a.release) ? -1 : 0));
+		}
+		
+		
+		
+		if(searchKey == false)
+		{
+			for (i = 0; i < movies.length; i++) //  For each movie in the database
+			{		
+				
+				if(reviews.length > 0)
+				{
+					var reviewsForThisMovie = [];
+					var totalRating = 0;
+					for (j = 0; j < reviews.length; j++) //  For each review in the database
+					{
+						if(movies[i].id == reviews[j].movie)
+						{
+							reviewsForThisMovie.push(reviews[j].rating);
+							totalRating += reviews[j].rating;
+						}
+					}
+					
+					if(reviewsForThisMovie.length > 0)
+					{
+						var avg = totalRating/reviewsForThisMovie.length;
+						document.getElementById("movieList").innerHTML +=
+						"<div class='movieWrapper'><img class='cover' src='" +
+						movies[i].image +
+						"' alt='" +
+						movies[i].title +  
+						" Cover'><p class='movieTitle'>" +
+						movies[i].title +
+						"</p><p class='movieText'>Director: " + 
+						movies[i].director +
+						"<br>Released: " + 
+						movies[i].release.toDateString().substring(4) +
+						"<br>Average Rating: " + avg + "/10 - " +
+						reviewsForThisMovie.length + " rating(s)" + 
+						"</p><a href='" + 
+						movies[i].imdb +
+						"' class='btn btn-primary' target='_blank'>IMDB Link</a><span> </span><button class='btn btn-primary' onclick=\"AddReview('" + 
+						movies[i].id +
+						"')\">Add Review</button></div><span> </span>";
+						
+					}
+					else
+					{
+						document.getElementById("movieList").innerHTML +=
+					"<div class='movieWrapper'><img class='cover' src='" +
+					movies[i].image +
+					"' alt='" +
+					movies[i].title +  
+					" Cover'><p class='movieTitle'>" +
+					movies[i].title +
+					"</p><p class='movieText'>Director: " + 
+					movies[i].director +
+					"<br>Released: " + 
+					movies[i].release.toDateString().substring(4) +
+					"<br>No Ratings for this movie" + 
+					"</p><a href='" + 
+					movies[i].imdb +
+					"' class='btn btn-primary' target='_blank'>IMDB Link</a><span> </span><button class='btn btn-primary' onclick=\"AddReview('" + 
+					movies[i].id +
+					"')\">Add Review</button></div><span> </span>";
+					}
+					
+					
+				}
+				
+				
+				
+				
+			
+			}
+		}
+		else
+		{
+			for (i = 0; i < movies.length; i++) //  For each movie in the database
+			{		
+				if(movies[i].title.toLowerCase().includes(searchKey.toLowerCase()) || movies[i].director.toLowerCase().includes(searchKey.toLowerCase()) || movies[i].id.toLowerCase().includes(searchKey.toLowerCase()))
+				{
+					document.getElementById("movieList").innerHTML +=
+					"<div class='movieWrapper'><img class='cover' src='" +
+					movies[i].image +
+					"' alt='" +
+					movies[i].title +  
+					" Cover'><p class='movieTitle'>" +
+					movies[i].title +
+					"</p><p class='movieText'>Director: " + 
+					movies[i].director +
+					"<br>Released: " + 
+					movies[i].release.toDateString().substring(4) +
+					"</p><a href='" + 
+					movies[i].imdb +
+					"' class='btn btn-primary' target='_blank'>IMDB Link</a><span> </span><button class='btn btn-primary' onclick=\"AddReview('" + 
+					movies[i].id +
+					"')\">Add Review</button></div><span> </span>";
+				}
+			
+			}
 		}
 	}
 }
@@ -105,6 +258,7 @@ function UpdateMovieList()
 function ChangeView(loc)
 {
 	document.getElementById("movies").classList.add("hidden");
+	document.getElementById("reviews").classList.add("hidden");
 	document.getElementById("newmovie").classList.add("hidden");
 	document.getElementById("newreview").classList.add("hidden");
 	document.getElementById("register").classList.add("hidden");
@@ -238,6 +392,23 @@ function AddNewUser()
 	}
 }
 
+function AddReview(movie)
+{
+	document.getElementById("movies").classList.add("hidden");
+	document.getElementById("newreview").classList.remove("hidden");
+	document.getElementById("newReviewMovie").value = movie;
+}
+
+function AddNewReview()
+{
+	var m = document.getElementById("newReviewMovie").value;
+	var r = document.getElementById("newReviewRating").value;
+	var t = document.getElementById("newReviewText").value;
+	var d = new Date();
+	
+	NewReviewToDB(d.getTime(),m,r,t); // ID, Movie, Rating, Text
+}
+
 function NewMovieToDB(id,t,d,r,im,img) // ID, Title, Director, Release Date (DD/MM/YYYY), IMDB Link, Image
 {
 	firebase.database().ref('movies/' + id).set({
@@ -250,7 +421,7 @@ function NewMovieToDB(id,t,d,r,im,img) // ID, Title, Director, Release Date (DD/
     if (error) {
       // The write failed...
     } else {
-		location.reload(); //Once the data was successfully passed, the page reloads.
+		ReloadPage(); //Once the data was successfully passed, the page reloads.
     }
   });
 }
@@ -265,9 +436,66 @@ function NewUserToDB(u,e,p) // Username, Email Address, Password
     if (error) {
       // The write failed...
     } else {
-		location.reload(); //Once the data was successfully passed, the page reloads.
+		ReloadPage(); //Once the data was successfully passed, the page reloads.
     }
   });
+}
+
+function NewReviewToDB(i,m,r,t) // ID, Movie, Rating, Text
+{
+		firebase.database().ref('reviews/' + i).set({
+    movie: m,
+    rating: r,
+    review: t,
+	user: user.name
+  }, function(error) {
+    if (error) {
+      // The write failed...
+    } else {
+		ReloadPage(); //Once the data was successfully passed, the page reloads.
+    }
+  });
+}
+
+function Login()
+{
+	var username = document.getElementById("loginUser").value;
+	var pass = document.getElementById("loginPass").value;
+	
+	
+	var userCorrect = false;
+	
+	for (i = 0; i < users.length; i++) //  For each user
+	{
+		if(users[i].name == username)
+		{
+			userCorrect = true;
+			if(users[i].pass == pass)
+			{
+				document.getElementById("loginUser").value = "";
+				user = users[i];
+				UpdateUI();
+			}
+			else
+			{
+				alert("Password is incorrect!");
+			}
+		}
+	}
+	
+	if(!userCorrect)
+	{
+		alert("User not found!");
+	}
+	
+	document.getElementById("loginPass").value = "";
+	
+}
+
+function Logout()
+{
+	user = "";
+	UpdateUI();
 }
 
 function ReloadPage()
@@ -301,4 +529,25 @@ function CheckNewMovie()
 	}
 		
 		imgPreview.src = img;
+}
+
+function ClearSearch()
+{
+	document.getElementById("searchBox").value = "";
+	searchKey = "";
+	search = false;
+	UpdateUI();
+}
+
+function Search()
+{
+	searchKey = document.getElementById("searchBox").value;
+	search = true;
+	UpdateUI();
+}
+
+function UpdateSort()
+{
+	sortOption = document.getElementById("selectSort").value;
+	UpdateUI();
 }
